db41bb41468f408a7e83de792a90540a39eee6b4
DenDen047 <sh.mn.nat@gmail.com>
2020-04-23 05:08:56 +0900

    success to show all dirs

--- useful_commands/make_zips.sh
+++ useful_commands/make_zips.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+counter(){
+    for dir in "$1"/*
+    do
+        if [ -d "$dir" ]   # is $dir a directory?
+        then
+            echo "$dir"
+            counter "$dir"
+        fi
+    done
+}
+
 cd ~/Downloads && \
-zip -r hoge.zip test/
+counter "test"
+# zip -r hoge.zip test/
