c98a2c4ecb76c954db01644168d0b46b9ee1715f
DenDen047 <sh.mn.nat@gmail.com>
2020-04-23 06:20:19 +0900

    complete to zip files

--- useful_commands/make_zips.sh
+++ useful_commands/make_zips.sh
@@ -4,22 +4,35 @@ make_zip(){
     root_dir=$(pwd)
 
     # make a zip file
-    zip_name=`echo $1 | awk -F "/" '{ print $NF }'`
-    cd $(dirname $1) && \
-    zip -r ${zip_name}.zip ${zip_name}
+    zip_name=`echo "$1" | awk -F "/" '{ print $NF }'`
+    cd "$(dirname "$1")" && \
+    zip -r "${zip_name}".zip "${zip_name}"
 
-    cd ${root_dir}
+    cd "${root_dir}"
 }
 
 make_zips(){
     for dir in "$1"/*
     do
-        if [ -d "$dir" ]   # is $dir a directory?
+        if [ -d "${dir}" ]   # is $dir a directory?
         then
-            make_zip ${dir}
+            not_include_dir=true
+            for d in "${dir}"/*
+            do
+                if [ -d "${d}" ]
+                then
+                    not_include_dir=false
+                fi
+            done
+
+            if "${not_include_dir}"
+            then
+                make_zip "${dir}"
+                rm -rf "${dir}"
+            fi
 
             # recursively
-            make_zips ${dir}
+            make_zips "${dir}"
         fi
     done
 }
