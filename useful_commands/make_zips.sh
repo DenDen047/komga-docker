9a6e26fbe74e72ebaa33088940ccb07eb8e80ca7
DenDen047 <sh.mn.nat@gmail.com>
2020-04-23 05:41:26 +0900

    spilt functions

--- useful_commands/make_zips.sh
+++ useful_commands/make_zips.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+make_zip(){
+    root_dir=$(pwd)
+
+    # make a zip file
+    zip_name=`echo $1 | awk -F "/" '{ print $NF }'`
+    cd $(dirname $1) && \
+    zip -r ${zip_name}.zip ${zip_name}
+
+    cd ${root_dir}
+}
+
 make_zips(){
     for dir in "$1"/*
     do
         if [ -d "$dir" ]   # is $dir a directory?
         then
-            root_dir=$(pwd)
-
-            # make a zip file
-            zip_name=`echo ${dir} | awk -F "/" '{ print $NF }'`
-            cd $(dirname ${dir}) && \
-            zip -r ${zip_name}.zip ${zip_name}
-
-            cd ${root_dir}
+            make_zip ${dir}
 
             # recursively
             make_zips ${dir}
@@ -20,5 +24,4 @@ make_zips(){
     done
 }
 
-cd ~/Downloads && \
-make_zips test
+make_zips $1
